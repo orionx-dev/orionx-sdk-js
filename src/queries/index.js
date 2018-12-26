24faae79f76535fa5ee2a92b81c29ee59f259202
Joaquin Diaz <joaco@Joaquins-MacBook-Pro.local>
2018-12-26 15:42:47 -0300

    added trades to the index

--- src/queries/index.js
+++ src/queries/index.js
@@ -2,10 +2,12 @@ import market from './market'
 import marketOrderBook from './marketOrderBook'
 import markets from './markets'
 import me from './me'
+import trades from './trades'
 
 export default {
   market,
   marketOrderBook,
   markets,
-  me
+  me,
+  trades
 }
