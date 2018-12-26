1943d8be0cd0fa71cdc964a808cfd987789b8fcb
Joaquin Diaz <joaco@Joaquins-MacBook-Pro.local>
2018-12-26 15:47:55 -0300

    added paginated params

--- src/queries/trades.js
+++ src/queries/trades.js
@@ -1,9 +1,21 @@
 import gql from 'graphql-tag'
 
-export default async function() {
+export default async function({ filter, page, limit, sortBy, sortType }) {
   const query = gql`
-    query {
-      trades {
+    query getTrades(
+      $filter: String
+      $page: Int
+      $limit: Int
+      $sortBy: String
+      $sortType: SortType
+    ) {
+      trades(
+        filter: $filter
+        page: $page
+        limit: $limit
+        sortBy: $sortBy
+        sortType: $sortType
+      ) {
         _id
         page
         totalCount
@@ -27,6 +39,9 @@ export default async function() {
       }
     }
   `
-  const response = await this.graphql({ query, variables: {} })
+  const response = await this.graphql({
+    query,
+    variables: { filter, page, limit, sortBy, sortType }
+  })
   return response.trades
 }
