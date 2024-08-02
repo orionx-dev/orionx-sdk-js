058e3fc5b939efac19a9472f8e5f1d466b527ba4
Joaquin Diaz <joaquin.diaz@ies.cl>
2024-08-02 01:20:22 -0400

    chore: Removed unused code

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,3 @@
 import Orionx from './orionx';
 
 export default Orionx;
-
-async function main() {
-  const orionx = new Orionx(
-    process.env.ORIONX_API_KEY,
-    process.env.ORIONX_SECRET_KEY,
-    process.env.ORIONX_API_ENDPOINT
-  );
-
-  const me = await orionx.user.getMe();
-  console.log(me);
-
-  let totalOrders = 10;
-  while (totalOrders > 0) {
-    const order = await orionx.placeLimitOrder();
-    console.log(order);
-    totalOrders -= 1;
-    if (totalOrders == 0) {
-      break;
-    }
-  }
-}
-
-main();
