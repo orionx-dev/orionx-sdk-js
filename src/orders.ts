e986f4cf857be41468fdae96578884309d2ad608
Joaquin Diaz <joaquin.diaz@ies.cl>
2024-08-02 21:16:24 -0400

    added more orders queries and mutations

--- src/orders.ts
+++ src/orders.ts
@@ -1,4 +1,5 @@
 import Api from './api';
+import { GetOrdersParameters } from './types';
 
 export default class Orders {
   private apiClient: Api;
@@ -7,6 +8,83 @@ export default class Orders {
     this.apiClient = apiClient;
   }
 
+  /**
+   * Queries
+   */
+
+  public async getOrder(orderId: string) {
+    const query = `
+      query getOrder($orderId: ID!) {
+        order(orderId: $orderId) {
+          _id
+          type
+          amount
+          limitPrice
+          stopPrice
+          status
+          createdAt
+          activatedAt
+          closedAt
+          market {
+            code
+            mainCurrency {
+              code
+              units
+            }
+            secondaryCurrency {
+              code
+              units
+            }
+          }
+          clientId
+        }
+      }
+    `;
+
+    const response = await this.apiClient.call(query, {
+      orderId,
+    });
+
+    return response;
+  }
+
+  public async getOrders(ordersParameters: GetOrdersParameters) {
+    const query = `
+      query getOrders($filter: String, $marketCode: String, $onlyOpen: Boolean, $onlyClosed: Boolean, $currencyCode: String, $onlyFilled: Boolean, $page: Int, $limit: Int, $sortBy: String, $sortType: String) {
+        orders(filter: $filter, marketCode: $marketCode, onlyOpen: $onlyOpen, onlyClosed: $onlyClosed, currencyCode: $currencyCode, onlyFilled: $onlyFilled, page: $page, limit: $limit, sortBy: $sortBy, sortType: $sortType) {
+          _id
+          type
+          amount
+          limitPrice
+          stopPrice
+          status
+          createdAt
+          activatedAt
+          closedAt
+          market {
+            code
+            mainCurrency {
+              code
+              units
+            }
+            secondaryCurrency {
+              code
+              units
+            }
+          }
+          clientId
+        }
+      }
+    `;
+
+    const response = await this.apiClient.call(query, ordersParameters);
+
+    return response;
+  }
+
+  /**
+   * Mutations
+   */
   public async placeLimitOrder(
     marketCode: string,
     amount: number,
@@ -92,6 +170,25 @@ export default class Orders {
       amount,
       sell,
       clientId,
+    });
+
+    return response;
+  }
+
+  public async cancelOrder(orderId: string) {
+    const query = `
+      mutation cancelOrder($orderId: ID!) {
+        cancelOrder(orderId: $orderId) {
+          _id
+          type
+          status
+          clientId
+        }
+      }
+    `;
+
+    const response = await this.apiClient.call(query, {
+      orderId,
     });
 
     return response;
