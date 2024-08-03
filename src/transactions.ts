4fe42735162151c522d09ded2b47ff2ed7bb60c2
Joaquin Diaz <joaquin.diaz@ies.cl>
2024-08-02 22:16:21 -0400

    added transactions

--- src/transactions.ts
+++ src/transactions.ts
@@ -1,7 +1,7 @@
 import Api from './api';
 import { GetTransactionsParameters } from './types';
 
-export default class Orders {
+export default class Transactions {
   private apiClient: Api;
 
   constructor(apiClient) {
@@ -100,5 +100,89 @@ export default class Orders {
     const response = await this.apiClient.call(query, parameters);
 
     return response;
+  }
+
+  /**
+   * Mutations
+   */
+
+  public async send(
+    fromWalletId: string,
+    contactId?: string,
+    network: string,
+    amount: number,
+    description?: string,
+    clientId?: string
+  ) {
+    const query = `
+      mutation sdk_send(
+        $fromWalletId: ID
+        $contactId: ID
+        $network: String
+        $amount: BigInt
+        $description: String
+        $clientId: ID
+      ) {
+        sendCrypto(
+          fromWalletId: $fromWalletId
+          contactId: $contactId
+          network: $network
+          amount: $amount
+          description: $description
+          clientId: $clientId
+        ) {
+          _id
+          type
+          amount
+          limitPrice
+          status
+          createdAt
+          market {
+            code
+          }
+          clientId
+        }
+      }
+    `;
+
+    const response = await this.apiClient.call(query, {
+      marketCode,
+      amount,
+      limitPrice,
+      sell,
+      clientId,
+    });
+
+    return response;
+  }
+
+  public async convert(
+    quoteOptionId: string,
+    amount: number,
+    marketCode: string,
+    sell: boolean
+  ) {
+    const query = `
+      mutation sdk_convert(
+        $quoteOptionId: ID
+        $amount: BigInt
+        $marketCode: ID
+        $sell: Boolean
+      ) {
+        instantTransaction(
+          quoteOptionId: $quoteOptionId
+          amount: $amount
+          marketCode: $marketCode
+          sell: $sell
+        )
+      }
+    `;
+
+    const response = await this.apiClient.call(query, {
+      quoteOptionId,
+      amount,
+      marketCode,
+      sell,
+    });
   }
 }
